4a28155a59d63b8d0b5546a4dd8a66f997378e61
Chew Lim <chew@ricemill.co>
2023-06-19 23:48:51 +1000

    type hint controller response

--- app/Http/Controllers/MultiplyMatricesController.php
+++ app/Http/Controllers/MultiplyMatricesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Actions\MultiplyMatrices;
 use App\Http\Requests\MultiplyMatricesRequest;
+use Illuminate\Http\JsonResponse;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -14,7 +15,7 @@ class MultiplyMatricesController extends Controller
         return Inertia::render('Index');
     }
 
-    public function store(MultiplyMatricesRequest $request)
+    public function store(MultiplyMatricesRequest $request): JsonResponse
     {
         $validated = $request->validated();
 
